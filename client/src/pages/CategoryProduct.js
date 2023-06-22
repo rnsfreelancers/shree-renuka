89ed7ff377af7da57f2d2825424f736aa57aec65
ranga chimalgi <rangachimalgi123@gmail.com>
2023-06-22 22:38:09 +0530

    modification in CategoryProduct

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -2,13 +2,10 @@ import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { Checkbox, Radio, Button, Modal } from "antd";
-import { Prices } from "../components/Prices";
-import { Sort } from "../components/Filter/Sort";
-import { useFilter } from "../context/filter-context";
+import { Checkbox, Button, Modal } from "antd";
 import { useCart } from "../context/cart";
-import { Brand } from "../components/Filter/Brand";
 import toast from "react-hot-toast";
+import { Brand } from "../components/Filter/Brand";
 import "./../assests/styles/Filter.css";
 
 const CategoryProduct = () => {
@@ -19,19 +16,16 @@ const CategoryProduct = () => {
   const [category, setCategory] = useState([]);
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
-  const [radio, setRadio] = useState([]);
   const [checkedBrands, setCheckedBrands] = useState([]);
   const [showFilters, setShowFilter] = useState(false); // Toggle Filter
   const [isMobileScreen, setIsMobileScreen] = useState(false); // Check for mobile screen
 
-  const { state } = useFilter();
-
   useEffect(() => {
-    if (params?.slug) getPrductsByCat();
+    if (params?.slug) getProductsByCat();
   }, [params?.slug]);
 
   useEffect(() => {
-    getAllCategory();
+    getAllCategories();
   }, []);
 
   useEffect(() => {
@@ -47,7 +41,7 @@ const CategoryProduct = () => {
     };
   }, []);
 
-  const getPrductsByCat = async () => {
+  const getProductsByCat = async () => {
     try {
       const { data } = await axios.get(
         `/api/v1/product/product-category/${params.slug}`
@@ -59,8 +53,7 @@ const CategoryProduct = () => {
     }
   };
 
-  //get all cat
-  const getAllCategory = async () => {
+  const getAllCategories = async () => {
     try {
       const { data } = await axios.get("/api/v1/category/get-category");
       if (data?.success) {
@@ -71,17 +64,6 @@ const CategoryProduct = () => {
     }
   };
 
-  //get products
-  const getAllProducts = async () => {
-    try {
-      const { data } = await axios.get(`/api/v1/product/get-product`);
-      setProducts(data.products);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // filter by cat
   const handleFilter = (value, id) => {
     let all = [...checked];
     if (value) {
@@ -92,20 +74,11 @@ const CategoryProduct = () => {
     setChecked(all);
   };
 
-  useEffect(() => {
-    if (!checked.length || !radio.length) getAllProducts();
-  }, [checked.length, radio.length]);
-
-  useEffect(() => {
-    if (checked.length || radio.length) filterProduct();
-  }, [checked, radio]);
-
-  //get filtered product
   const filterProduct = async () => {
     try {
       const { data } = await axios.post("/api/v1/product/product-filters", {
         checked,
-        radio,
+        checkedBrands,
       });
       setProducts(data?.products);
     } catch (error) {
@@ -113,30 +86,6 @@ const CategoryProduct = () => {
     }
   };
 
-  // Filter by Shubham
-
-  const getSortedProducts = (products, sort) => {
-    const sortedProducts = [...products].sort((product1, product2) =>
-      sort === "lth"
-        ? product1.price - product2.price
-        : sort === "htl"
-        ? product2.price - product1.price
-        : products
-    );
-    return sortedProducts;
-  };
-
-  const getProductByBrand = (products, brand) => {
-    const brandProducts = products.filter((product) =>
-      brand.length > 0 ? brand.includes(product.brand) : products
-    );
-    return brandProducts;
-  };
-
-  const sortedProducts = getSortedProducts(products, state.sort);
-  const brandProducts = getProductByBrand(products, state.brand);
-
-  // Filter Modal
   const openFiltersModal = () => {
     setShowFilter(true);
   };
@@ -163,114 +112,130 @@ const CategoryProduct = () => {
             <label className="text-center category-filter-heading">
               Category
             </label>
-            <div className="d-flex flex-column">
-              {categories?.map((c) => (
-                <Checkbox
-                  key={c._id}
-                  onChange={(e) => handleFilter(e.target.checked, c._id)}
-                >
-                  {c.name}
-                </Checkbox>
+            <div className="d-flex flex-wrap category-container">
+              {categories.map((cat) => (
+                <div key={cat._id} className="filter-category">
+                  <Checkbox
+                    onChange={(e) => handleFilter(e.target.checked, cat._id)}
+                    checked={checked.includes(cat._id)}
+                  >
+                    {cat.name}
+                  </Checkbox>
+                </div>
               ))}
             </div>
             <hr></hr>
-            <label className="text-center category-filter-heading">Brand</label>
-
-            <Brand />
+            <label className="text-center category-filter-heading">
+              Brands
+            </label>
+            <div className="filter-brand">
+              <Brand
+                setCheckedBrands={setCheckedBrands}
+                checkedBrands={checkedBrands}
+              />
+            </div>
+            <Button
+              className="filter-button"
+              onClick={filterProduct}
+              type="primary"
+              block
+            >
+              Apply Filter
+            </Button>
           </div>
         )}
-
-        {/* Toggle button for mobile screens */}
-        {isMobileScreen && (
-          <div className="d-sm-none text-center mb-3">
-            <Button onClick={openFiltersModal}>Show Filters</Button>
-          </div>
-        )}
-
-        <div className="col-sm-9">
-          <h6 className="text-center">{products?.length} result found </h6>
-          <div className="d-flex flex-wrap">
-            {brandProducts?.map((p) => (
-              <div
-                className="card m-2"
-                style={{ width: "18rem" }}
-                key={p._id}
-              >
-                <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
-                  className="card-img-top"
-                  alt={p.name}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">
-                    {p.description.substring(0, 30)}...
-                  </p>
-                  <p className="card-text"> $ {p.price}</p>
-                  <button
-                    className="btn btn-primary ms-1"
-                    onClick={() => navigate(`/product/${p.slug}`)}
-                  >
-                    More Details
-                  </button>
-                  <button
-                    className="btn btn-secondary ms-1"
-                    onClick={() => {
-                      setCart([...cart, p]);
-                      localStorage.setItem(
-                        "cart",
-                        JSON.stringify([...cart, p])
-                      );
-                      toast.success("Item Added to cart");
-                    }}
-                  >
-                    ADD TO CART
-                  </button>
+        <div className={isMobileScreen ? "col-sm-12" : "col-sm-9"}>
+          <div className="container mt-3">
+            <h4 className="text-center">Category - {category?.name}</h4>
+            <h6 className="text-center">{products?.length} result found</h6>
+            <div className="row">
+              <div className="col-md-9 offset-1">
+                <div className="d-flex flex-wrap products-container">
+                  {products?.map((p) => (
+                    <div
+                      className="card m-2 new-product-card clickable-pointer"
+                      key={p._id}
+                      onClick={() => navigate(`/product/${p.slug}`)}
+                    >
+                      <img
+                        src={`/api/v1/product/product-photo/${p._id}`}
+                        className="card-img-top"
+                        alt={p.name}
+                      />
+                      <div className="card-body">
+                        <h5>{p.name}</h5>
+                        <p className="card-text">
+                          {p.description.substring(0, 30)}...
+                        </p>
+                        <div className="card-buttons">
+                          <button
+                            className="btn btn-secondary ms-1"
+                            onClick={() => {
+                              setCart([...cart, p]);
+                              localStorage.setItem(
+                                "cart",
+                                JSON.stringify([...cart, p])
+                              );
+                              toast.success("Item Added to cart");
+                            }}
+                          >
+                            ADD TO CART
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-            ))}
+            </div>
           </div>
         </div>
       </div>
-
-      {/* Filter Modal */}
-      {isMobileScreen && (
-        <Modal
-          title="Filters"
-          visible={showFilters}
-          onOk={closeFiltersModal}
-          onCancel={closeFiltersModal}
-          footer={[
-            <Button key="clear" onClick={() => window.location.reload()}>
-          Clear
-        </Button>,
-            <Button key="submit" type="primary" onClick={closeFiltersModal}>
-              Apply Filters
-            </Button>,
-          ]}
-        >
-          <hr></hr>
-          <div className="d-flex flex-column">
-            <label className="text-left category-filter-heading">
-              Category
-            </label>
-            <div className="d-flex flex-column">
-              {categories?.map((c) => (
-                <Checkbox
-                  key={c._id}
-                  onChange={(e) => handleFilter(e.target.checked, c._id)}
-                >
-                  {c.name}
-                </Checkbox>
-              ))}
+      <Modal
+        title="Filters"
+        visible={showFilters}
+        onCancel={closeFiltersModal}
+        footer={null}
+        width={isMobileScreen ? "100%" : 800}
+        destroyOnClose={true}
+      >
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <label className="filter-label">Categories</label>
+              <div className="d-flex flex-wrap filter-container">
+                {categories.map((cat) => (
+                  <div key={cat._id} className="filter-category">
+                    <Checkbox
+                      onChange={(e) => handleFilter(e.target.checked, cat._id)}
+                      checked={checked.includes(cat._id)}
+                    >
+                      {cat.name}
+                    </Checkbox>
+                  </div>
+                ))}
+              </div>
             </div>
-            <hr />
-            <label className="text-left category-filter-heading">Brand</label>
-
-            <Brand />
+            <div className="col-md-6">
+              <label className="filter-label">Brands</label>
+              <div className="filter-brand">
+                <Brand
+                  setCheckedBrands={setCheckedBrands}
+                  checkedBrands={checkedBrands}
+                />
+              </div>
+            </div>
           </div>
-        </Modal>
-      )}
+          <Button
+            className="filter-button"
+            onClick={filterProduct}
+            type="primary"
+            block
+          >
+            Apply Filter
+          </Button>
+        </div>
+      </Modal>
     </Layout>
   );
 };
