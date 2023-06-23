66632f13aacb9e9ea4518d7e5a2717764cbb9297
ranga chimalgi <rangachimalgi123@gmail.com>
2023-06-23 13:20:52 +0530

    changes in Search js file

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,29 +1,33 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
 import { useSearch } from "../context/search";
-import { useNavigate } from "react-router-dom";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
-
+import { useNavigate } from "react-router-dom";
+import "../assests/styles/Search.css";
 
 const Search = () => {
   const [values, setValues] = useSearch();
-  const navigate = useNavigate();
   const [cart, setCart] = useCart();
+  const navigate = useNavigate();
 
   return (
     <Layout title={"Search results"}>
       <div className="container">
         <div className="text-center">
-          <h1>Search Resuts</h1>
+          <h1>Search Results</h1>
           <h6>
             {values?.results.length < 1
               ? "No Products Found"
               : `Found ${values?.results.length}`}
           </h6>
-          <div className="d-flex flex-wrap mt-4">
+          <div className="d-flex flex-wrap search-container">
             {values?.results.map((p) => (
-              <div className="card m-2" style={{ width: "18rem" }}>
+              <div
+                className="card m-2 search-card clickable-pointer"
+                key={p._id}
+                onClick={() => navigate(`/product/${p.slug}`)}
+              >
                 <img
                   src={`/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
@@ -34,26 +38,18 @@ const Search = () => {
                   <p className="card-text">
                     {p.description.substring(0, 30)}...
                   </p>
-                  <p className="card-text"> $ {p.price}</p>
-                  <button
-                    className="btn btn-primary ms-1"
-                    onClick={() => navigate(`/product/${p.slug}`)}
-                  >
-                    More Details
-                  </button>
-                  <button
-                    className="btn btn-secondary ms-1"
-                    onClick={() => {
+                  <div className="card-buttons">
+                    <button className="btn btn-secondary ms-1" onClick={() => {
                       setCart([...cart, p]);
                       localStorage.setItem(
                         "cart",
                         JSON.stringify([...cart, p])
                       );
                       toast.success("Item Added to cart");
-                    }}
-                  >
-                    ADD TO CART
-                  </button>
+                    }}>
+                      ADD TO CART
+                    </button>
+                  </div>
                 </div>
               </div>
             ))}
