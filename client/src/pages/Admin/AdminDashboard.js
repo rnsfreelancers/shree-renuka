a508920d75191962dd7dadd475a7c1484781fcd5
Ranga <rangachimalgi123@gmail.com>
2024-02-19 12:42:48 +0530

    import keep alive in server.js

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -16,7 +16,7 @@ const AdminDashboard = () => {
               <h3> Admin Name : {auth?.user?.name}</h3>
               <h3> Admin Email : {auth?.user?.email}</h3>
               <h3> Admin Contact : {auth?.user?.phone}</h3>
-            </div>
+            </div> 
           </div>
         </div>
       </div>
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
 import { fileURLToPath } from 'url';
-import "./keepAlive.js";
+import "./keepAlive.js"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
